57c28c7c965587f6b8067f01e0a3a1ae314ea1cd
desarrolloafs2 <desarrolloafs2@gmail.com>
2025-10-17 08:57:48 +0100

    configuration

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import './index.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className="bg-blue-500 text-white">
+        Rakel
+      </div>
+    </>
+  )
+}
+
+export default App
